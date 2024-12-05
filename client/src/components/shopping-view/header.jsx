1f5bf203020bee9bc561750cdb7a45c0ad961f7a
Pearn203 <kog151603@gmail.com>
2024-12-05 19:57:55 +0700

    feat:create layout and route

---
+++ client/src/components/shopping-view/header.jsx
@@ -0,0 +1,6 @@
+function ShoppingHeader(){
+    return(
+        <div> Shopping view Header</div>
+    );
+}
+export default ShoppingHeader;
---
+++ client/src/pages/shopping-view/account.jsx
@@ -0,0 +1,6 @@
+function ShoppingAccount() {
+    return <div>shopping view account</div>;
+  }
+  
+  export default ShoppingAccount;
+  
