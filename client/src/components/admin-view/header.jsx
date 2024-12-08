37ea330d7be1e66c083efea7a2337e7a3986fa8f
HoangHuy1305HP <huyhoangyii@gmail.com>
2024-12-08 15:35:22 +0700

    feat/View admin

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,11 +1,20 @@
+import { Button } from "../ui/button";
 
-
-function AdminHeader(){
-    return (
-        <div>
-            admin header
+function AdminHeader(setOpen){
+    return <header className="flex items-center justify-between px-4">
+        <button onClick={() => setOpen(true)} className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+        </button>
+        <div className="flex flex-1 justify-end">
+            <button className='inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow'>
+            <LogOut />
+            Logout
+            </button>
         </div>
-    );
+    </header>
+        
+    
 
 }
 export default AdminHeader;
