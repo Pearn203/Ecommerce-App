779ed390de4be54ef7a5fdd6108b02401df49961
HoangHuy1305HP <huyhoangyii@gmail.com>
2024-12-30 16:29:09 +0700

    update code shopping view account page

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,19 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
 import commonFeatureSlice from "./common-slice";
-
+import shopAddressSlice from "./shop/address-slice";
 import shopProductsSlice from "./shop/products-slice";
 import shoppingCartSlice from "./shop/cart-slice";
 import reviewSlice from "./shop/review-slice";
-
+import shopOrderSlice from "./shop/order-slice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductsSlice,
     shopProducts: shopProductsSlice,
     shopCart: shoppingCartSlice,
+    shopAddress: shopAddressSlice,
     shopReview: reviewSlice,
-    commonFeature: commonFeatureSlice
+    commonFeature: commonFeatureSlice,
+    shopOrder: shopOrderSlice,
   },
 });
 
